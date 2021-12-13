a71c46d7526bd18042be083aa05ecd78d90d8437
RaulTheormech <93100573+RaulTheormech@users.noreply.github.com>
2021-12-13 12:28:26 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -49,34 +49,8 @@ int main(int argc, char **argv) {
         }
       }
       std::cout << "Leftmost: " << coord_x[left] << " " << coord_y[left] << std::endl;
-      std::cout << "Rightmost: " << coord_x[right] << " " << coord_y[right] <<
-                std::endl;
-
-//      // вариант 2
-//      float max_left = 0, max_right = 0;
-//      for (int i = 0; i < coord_x.size(); ++i) {
-//        float new_x = cos_alpha * coord_x[i] + sin_alpha * coord_y[i];
-//        float new_y = -sin_alpha * coord_x[i] + cos_alpha * coord_y[i];
-
-//        if (new_y > max_left) {
-//          max_left = new_y;
-//          left = i;
-//        }
-//        if (new_y < max_right) {
-//          max_right = new_y;
-//          right = i;
-//        }
-//      }
-//      std::cout << "Case 2" << std::endl;
-//      std::cout << "Leftmost: " << coord_x[left] << " " << coord_y[left] << std::endl;
-//      std::cout << "Rightmost: " << coord_x[right] << " " << coord_y[right] <<
-//                std::endl;
-    } else {
-      std::cout << "Unable to open file" << std::endl;
-    }
-  } else {
-    std::cout << "there are no arguments or there are more of them than we expect"
-              << std::endl;
-  }
+      std::cout << "Rightmost: " << coord_x[right] << " " << coord_y[right] << std::endl;
+    } 
+  } 
   return 0;
 }
