c34fe7a5caf318379314c76c5d802563b1ff16f8
RaulTheormech <93100573+RaulTheormech@users.noreply.github.com>
2021-12-16 17:19:03 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -3,13 +3,59 @@
 #include <vector>
 
 using namespace std;
+const float g = 9.81;
+
+int traectories(float x_0, float h_0, float vx0, float vy0,
+                int start_idx,
+                const std::vector<float> &coord_x,
+                const std::vector<float> &coord_y) {
+  if (vx0 > 0) {
+    int new_idx = start_idx + 1;
+    float xi = coord_x[new_idx];
+    float ti = xi - x_0 / vx0;
+    float hi = -0.5 * g * ti * ti + vy0 * ti + h_0;
+
+    if (hi <= 0) {
+      return start_idx;
+    } else if (coord_y[new_idx] >= hi) {
+      float vxi = -vx0;
+      float vyi = -g * ti + vy0;
+      return traectories(xi, hi, vxi, vyi, new_idx, coord_x, coord_y);
+    } else {
+      if (new_idx == coord_x.size() - 1) {
+        return new_idx;
+      }
+      float vxi = vx0;
+      float vyi = -g * ti + vy0;
+      return traectories(xi, hi, vxi, vyi, new_idx, coord_x, coord_y);
+    }
+  }
+
+  if (vx0 < 0) {
+    int new_idx = start_idx - 1;
+    float xi = coord_x[new_idx];
+    float ti = xi - x_0 / vx0;
+    float hi = -0.5 * g * ti * ti + vy0 * ti + h_0;
+
+    if (hi <= 0) {
+      return new_idx;
+    } else if (coord_y[new_idx] >= hi) {
+      float vxi = -vx0;
+      float vyi = -g * ti + vy0;
+      return traectories(xi, hi, vxi, vyi, new_idx, coord_x, coord_y);
+    } else {
+      float vxi = vx0;
+      float vyi = -g * ti + vy0;
+      return traectories(xi, hi, vxi, vyi, new_idx, coord_x, coord_y);
+    }
+  }
+}
 
 int main(int argc, char **argv) {
-  const float g = 9.81;
   if(argc == 2) {
     // есть один агрумент
     // в argv[1] содержится строка с первым агрументом (имя файла)
-    //std::cout << "input filename: " << argv[1] << std::endl;
+//    std::cout << "input filename: " << argv[1] << std::endl;
     string line;
     fstream myfile(argv[1]);
     if (myfile.is_open()) { //проверка открытия файла
@@ -19,8 +65,10 @@ int main(int argc, char **argv) {
       myfile >> dy;
       int i = 0;
       float temp_coord;
-      vector<float> coord_x; // координаты перегородок
-      vector<float> coord_y;
+      std::vector<float> coord_x; // координаты перегородок
+      std::vector<float> coord_y;
+      coord_x.push_back(0);
+      coord_y.push_back(h0);
       while (myfile >> temp_coord) {
         if (i % 2 == 0) {
           coord_x.push_back(temp_coord);
@@ -31,25 +79,33 @@ int main(int argc, char **argv) {
       }
       myfile.close();
 
-      // переходим к расчету
-      float hi = h0;
-      float ti = 0;
-      bool is_find = false;
-      for (int i = 0; i < coord_x.size(); ++i) {
-        ti = coord_x[i] / dx;
-        hi = -0.5 * g * ti * ti + dy * ti + h0;
-        if (hi < coord_y[i] || hi <= 0) {
-          cout << i << endl;
-          is_find = true;
-          break;
-        }
-      }
-      if (is_find != true) {
-        cout << coord_x.size()
-                  << std::endl;
-      }
+      int idx = traectories(0, h0, dx, dy, 0, coord_x, coord_y);
+      std::cout << idx;
 
-    } 
-  } 
+//      // переходим к расчету
+//      float hi = h0;
+//      float ti = 0;
+//      bool is_find = false;
+//      for (int i = 0; i < coord_x.size(); ++i) {
+//        ti = coord_x[i] / dx;
+//        hi = -0.5 * g * ti * ti + dy * ti + h0;
+//        if (hi < coord_y[i] || hi <= 0) {
+//          std::cout << "mat. the point will land on the site " << i - 1 << std::endl;
+//          is_find = true;
+//          break;
+//        }
+//      }
+//      if (is_find != true) {
+//        std::cout << "mat. the point will land on the site " << coord_x.size()
+//                  << std::endl;
+//      }
+
+    } else {
+      std::cout << "Unable to open file" << std::endl;
+    }
+  } else {
+    std::cout << "there are no arguments or there are more of them than we expect"
+              << std::endl;
+  }
   return 0;
 }
