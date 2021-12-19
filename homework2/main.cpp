aa1e471a73ad5c9b0009afde4a1b1ad53cb58b41
RaulTheormech <93100573+RaulTheormech@users.noreply.github.com>
2021-12-19 20:03:08 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -81,7 +81,7 @@ int main(int argc, char **argv) {
       myfile.close();
       
       if (coord_x.size() <= 1) {
-       return 0; 
+       return -1; 
       }
       int idx = traectories(0, h0, dx, dy, 0, coord_x, coord_y);
       std::cout << idx;
